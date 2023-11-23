de68cc16b150645887dfbcbbabb914e113fa3b08
Jack Papel <jackpapel@live.com>
2023-11-23 01:51:33 -0500

    Styling for members with linked sites

--- common/common.js
+++ common/common.js
@@ -24,19 +24,20 @@ class EguPersonCardElement extends HTMLElement {
         fetch(`/common/templates/egu-card.html`)
             .then(res => res.text())
             .then(text => {
+                const href = this.attributes.getNamedItem('href')?.textContent;
+
                 this.outerHTML = text
-                    .replace('$href', this.attributes.getNamedItem('href')?.textContent ?? 'null')
+                    .replace('$href', href ? `href=${href}` : '')
                     .replace('$img-src', this.attributes.getNamedItem('img-src')?.textContent ?? 'null')
-                    .replace('$inner', this.innerHTML)
+                    .replace('$inner', this.innerHTML);
             })
             .then(() => {
                 // TECHNICALLY for n elements, this runs O(n^2) times but it's fast enough
                 // that I dont care.
                 document.querySelectorAll(".egu-card").forEach(el => {
-                    if (el.attributes.getNamedItem('href').textContent !== 'null') {
-                        el.addEventListener('click', () => {
-                            window.location = el.attributes.getNamedItem('href').textContent;
-                        })
+                    const href = el.attributes.getNamedItem('href')?.textContent;
+                    if (href) {
+                        el.addEventListener('click', () => window.location = href)
                     }
                 });
             })
