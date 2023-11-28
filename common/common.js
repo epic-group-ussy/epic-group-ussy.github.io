f1978af9eb3f81fc983db03a799a8975cdf7e5c0
Jack Papel <jackpapel@live.com>
2023-11-28 12:07:21 -0500

    Simplify templates

--- common/common.js
+++ common/common.js
@@ -1,59 +1,38 @@
-
-class EguMainElement extends HTMLElement {
-    constructor() {
-        super();
-    }
-
-    connectedCallback() {
-        fetch(`/common/templates/egu-main.html`)
-            .then(res => res.text())
-            .then(text => {
-                this.outerHTML = text.replace('$inner', this.innerHTML)
-            })
-            .then(() => {
-                if (!document.head.innerHTML.includes('egu-main.css')) {
-                    document.head.innerHTML += `
-                        <link rel="stylesheet" type="text/css" href="/common/templates/egu-main.css">
-                    `;
-                }
-            })
-    }
+function appendStyleSheet(filePath) {
+    if (document.head.innerHTML.includes(filePath)) return;
+    document.head.innerHTML += `<link rel="stylesheet" type="text/css" href="${filePath}">`;
 }
 
-customElements.define('egu-main', EguMainElement);
+/**
+ * Defines a custom HTML element.
+ * 
+ * @param {string} tag The tag of the custom element
+ * @param {{[key: string]: string}} replacements Strings to replace
+ */
+function defineTemplate(tag, replacements = {}) {
+    const path = `/common/templates/${tag}`;
 
-class EguPersonCardElement extends HTMLElement {
-    constructor() {
-        super();
-    }
+    customElements.define(tag, class extends HTMLElement {
+        connectedCallback() {
+            appendStyleSheet(`${path}.css`);
 
-    connectedCallback() {
-        fetch(`/common/templates/egu-card.html`)
-            .then(res => res.text())
-            .then(text => {
-                const href = this.attributes.getNamedItem('href')?.textContent;
-
-                this.outerHTML = text
-                    .replace('$href', href ? `href=${href}` : '')
-                    .replace('$img-src', this.attributes.getNamedItem('img-src')?.textContent ?? '/members/gabes-father.jpg')
-                    .replace('$inner', this.innerHTML);
-            })
-            .then(() => {
-                // TECHNICALLY for n elements, this runs O(n^2) times but it's fast enough
-                // that I dont care.
-                document.querySelectorAll(".egu-card").forEach(el => {
-                    const href = el.attributes.getNamedItem('href')?.textContent;
-                    el.addEventListener('click', () => window.location = href ?? '/members/wip')
-                });
-            })
-            .then(() => {
-                if (!document.head.innerHTML.includes('egu-card.css')) {
-                    document.head.innerHTML += `
-                        <link rel="stylesheet" type="text/css" href="/common/templates/egu-card.css">
-                    `;
-                }
-            })
-    }
+            fetch(`${path}.html`)
+                .then(res => res.text())
+                .then(text => text.replace('$inner', this.innerHTML))
+                .then(text => {
+                    Object.entries(replacements).forEach(([key, backup]) => {
+                        text = text.replace(`$${key}`, this.getAttribute(key) ?? backup)
+                    })
+                    return text;
+                })
+                .then(text => this.outerHTML = text)
+        }
+    });
 }
 
-customElements.define('egu-card', EguPersonCardElement);
+defineTemplate('egu-main');
+
+defineTemplate('egu-card', {
+    href: '/members/wip',
+    ['img-src']: '/members/gabes-father.jpg'
+});
